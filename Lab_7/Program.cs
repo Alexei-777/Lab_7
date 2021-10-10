53e30c48c0d469c4566fbab0d726f1c521e1de54
Alexei.andriyanov@gmail.com <Alexei.andriyanov@gmail.com>
2021-10-10 10:08:15 +0300

    Добавьте файлы проекта.

---
+++ Lab_7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше (создать метод для вычисления  площади  треугольника по длинам его сторон).
+//Для решения задачи можно использовать формулу Герона , где x, y, z – стороны треугольника, p – полупериметр.
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество треугольников ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[N];
+            int[] y = new int[N];
+            int[] z = new int[N];
+            Vvod(ref N, out x, out y, out z);
+            Ploshad(N, x, y, z);
+            Console.ReadKey();
+        }
+        static void Vvod(ref int N, out int[] x, out int[] y, out int[] z)
+        {
+            x = new int[N];
+            y = new int[N];
+            z = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Введите длинну стороны {i + 1} треугольника X ");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Введите длинну стороны {i + 1} треугольника Y ");
+                y[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Введите длинну стороны {i + 1} треугольника Z ");
+                z[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void Ploshad(int N, int[] x, int[] y, int[] z)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                int P = (x[i] + y[i] + z[i]) / 2;
+                double S = Math.Sqrt(P * (P - x[i]) * (P - y[i]) * (P - z[i]));
+                Console.WriteLine($"Площадь треугольника {i + 1} равна {S.ToString("0.##")}");
+            }
+        }
+    }
+}
---
+++ Lab_7_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+
+namespace Lab_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба:");
+            int a = Int32.Parse(Console.ReadLine());
+            double Q =0;
+            double P = 0;
+            Kub(out Q, out P, a);
+            Console.WriteLine($"Обьем куба: {Q} куб.м");
+            Console.WriteLine($"Площадь поверхности: {P} кв.м");
+            Console.ReadKey();
+        }
+        static void Kub(out double Q,out double P,int a) 
+        { 
+            Q= Math.Pow(a, 3);
+            P= Math.Pow(a, 2) * 6;
+        }
+        
+        static void Kub2(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Обьем куба: {Math.Pow(a, 3)} куб.м");
+            Console.WriteLine($"Площадь поверхности: {Math.Pow(a, 2) * 6} кв.м");
+            Console.ReadKey();
+        }
+            
+
+    }
+
+}
